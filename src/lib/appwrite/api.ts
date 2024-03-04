1acf88605d6ba5b5a3560c7e85ed0feac823d2a3
Hareesh <hareeshnadakuditi5620@gmail.com>
2024-03-04 11:20:32 +0530

    api config signupform files changed

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from '@/types'
 import { ID } from 'appwrite'
-import { account } from './config'
+import appwriteConfig, { account, avatars, databases } from './config'
 
 const createUserAccount = async (user: INewUser) => {
   try {
@@ -10,7 +10,16 @@ const createUserAccount = async (user: INewUser) => {
       user.password,
       user.name
     )
-    return newAccount
+    if (!newAccount) throw Error
+    const avatarUrl = avatars.getInitials(user.name)
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    })
+    return newUser
   } catch (error) {
     console.log(error)
     return error
@@ -18,3 +27,23 @@ const createUserAccount = async (user: INewUser) => {
 }
 
 export default createUserAccount
+
+export async function saveUserToDB(user: {
+  accountId: string
+  email: string
+  name: string
+  imageUrl: URL
+  username?: string
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    )
+    return newUser
+  } catch (err) {
+    console.log(err)
+  }
+}
